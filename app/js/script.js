dc2b858032687f4137c6008f3b53b3bbc7a4b062
Yunuskhon Egamberdiev <yunusxon@gmail.com>
2021-04-14 20:59:46 +0500

    ref

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("works");
+const burgerMenu = document.querySelector('#burgerMenu');
+const header = document.querySelector('header.header')
+const overlay = document.querySelector('.overlay');
+burgerMenu.addEventListener('click', function (){
+   console.log('open hamburger');
+   if (header.classList.contains('open')) { // Close Burger menu
+       header.classList.remove('open');
+       overlay.classList.add('fade-out');
+   }
+   else { // Open Hamburger menu
+       header.classList.add('open');
+       overlay.classList.add('fade-in')
+   }
+
+});
