8521d2d8880d42d301d2efcbc873265d012ac89d
Yunuskhon Egamberdiev <yunusxon@gmail.com>
2021-04-14 23:41:05 +0500

    ref

--- app/js/script.js
+++ app/js/script.js
@@ -5,11 +5,12 @@ burgerMenu.addEventListener('click', function (){
    console.log('open hamburger');
    if (header.classList.contains('open')) { // Close Burger menu
        header.classList.remove('open');
+       overlay.classList.remove('fade-in');
        overlay.classList.add('fade-out');
    }
    else { // Open Hamburger menu
        header.classList.add('open');
-       overlay.classList.add('fade-in')
+       overlay.classList.remove('fade-out');
+       overlay.classList.add('fade-in');
    }
-
 });
